bb688e2e51f41e3f4bff4dfd49d86eb3369a6bb1
BNzioka <benjaminn.nzioka@gmail.com>
2023-02-24 03:12:16 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,11 +10,11 @@ int main(void)
 
 	int i;
 
-	printf("infinite loop incoming : (\n");
+	printf("infinite loop incoming :(\n");
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
-	/*	putchar(i);*/
+	/*putchar(i);*/
 	/*}*/
 	printf("infinite loop avoided! \\o/\n");
 	return (0);
