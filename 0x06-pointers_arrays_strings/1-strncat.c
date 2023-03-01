b89de54331659bcc3f061111026a6f1c5124d5ce
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-01 12:22:12 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,16 +2,22 @@
 
 /**
  * _strncat - A program that concatenates two strings using
+ *
  * at most n bytes from src
+ *
  * @dest: input value
+ *
  * @src: input value
+ *
  * @n: input value
- * Return: dest
+ *
+ * Return: A pointer to the string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
