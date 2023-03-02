0f34c704c659dcb7e05c8b2a274875ace270900f
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-02 12:17:38 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -19,5 +19,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar(n1 % 10) + '0';
+	_putchar((n1 % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -5,9 +5,9 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, an errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
