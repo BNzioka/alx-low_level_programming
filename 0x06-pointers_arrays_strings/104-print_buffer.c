9da02071ee4b81d56af87ac6eb26639229a531c1
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-05 11:28:39 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,7 +13,6 @@ void print_buffer(char *b, int size)
 	int o, j, i;
 
 	o = 0;
-
 	if (size <= 0)
 	{
 		printf("\n");
@@ -25,25 +24,25 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf(" ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++);
+		if (i < j)
+			printf("%02x", *(b + o + i));
+		else
+			printf(" ");
+		if (i % 2)
 		{
-			int c = *(b + o + i)
-
-			if (c < 32 || c > 132)
-			{
-				c = '.',
-			}
-			printf("%c", c);
+			printf(" ");
 		}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+		if (c < 32 || c > 132)
+		{
+			c = '.';
+		}
+			printf("%c", c);
+	}
 		printf("\n");
 		o += 10;
 	}
