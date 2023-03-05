802f555a978b90a09fe33f6513890f0e9eee14b4
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-05 10:07:10 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,6 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str:The string to be capitalized.
- *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -30,6 +29,8 @@ char *cap_string(char *str)
 		str[index - 1] == '}' ||
 		index == 0)
 		str[index] -= 32;
+
+		index++;
 	}
 	return (str);
 }
