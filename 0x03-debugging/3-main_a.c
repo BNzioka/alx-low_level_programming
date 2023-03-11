325d929c20aa1ce6ee78c0484d5380e771d7702b
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-11 12:05:40 -0800

    quick fix

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -13,13 +13,11 @@ int main(void)
 	int day;
 	int year;
 
-	month = 2;
-	day = 29;
-	year = 2000;
+	month = 4;
+	day = 10;
+	year = 1997;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
 
