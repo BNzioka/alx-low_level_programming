805b19fc46f2eeb65ea6647b3be0c01ad6940e7b
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-13 06:55:21 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || str == NULL)
 		return (NULL);
 
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 		str[i] = c;
 
 	return (str);
