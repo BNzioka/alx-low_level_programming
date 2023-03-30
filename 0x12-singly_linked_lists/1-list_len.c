eb127610c950fa5e5484f11b25e9cab019a7798b
BNzioka <benjaminn.nzioka@gmail.com>
2023-03-30 12:56:45 -0700

    file 0 and 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * list_len - returns number of elements in a linked list
+  *
+  * @h: linked list
+  * Return: number of elements in h
+  */
+size_t list_len(const list_t *h)
+{
+	if (h && h->next)
+		return (1 + list_len(h->next));
+	else if (h)
+		return (1);
+	return (0);
+}
